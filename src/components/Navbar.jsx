11c284097aa964900c74717596b75d27c3d69efe
Divyansh Sharma <divyanshsharma0301@gmail.com>
2024-12-23 15:35:01 +0530

    margin correction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className=" mb-20 flex items-center justify-between py-6 px-6">
+    <nav className=" mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center ">
         <img src={logo} alt="logo" className="w-14" />
       </div>
